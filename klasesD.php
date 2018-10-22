9cd76547e1eb84de9bde739a9beeccfc060f95c8
Laurencijus <zanaborovikaite@gmail.com>
2018-10-22 20:08:43 +0300

    initial commit

---
+++ klasesD.php
@@ -0,0 +1,9 @@
+<?php
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 5 == 0) {
+        echo $i . '';
+    }
+}
+for ($i = 2; $i < PHP_INT_MAX; $i *= 2) {
+    echo $i . '<br>';
+}
---
+++ task.php
@@ -0,0 +1,21 @@
+<html>
+    <body>
+        <table border= '1'>
+            <thead>
+                <tr>
+                    <th>ASCII numeris</th>
+                    <th>ASCII serija</th>
+</tr>
+<thead>
+    <tbody>
+        <?php
+for ($i = 32; $i <= 127; $i++) {
+    $symbol = chr($i);
+    echo "<tr><td><$1</td><td>$symbol</td></tr>";
+
+}
+?>
+         </tbody>
+    </table>
+    </body>
+    </html>
